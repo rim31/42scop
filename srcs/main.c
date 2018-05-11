be02b85d7359f32ca71e0b10dfd61547fd6e34bd
OSENG <oseng@vente-privee.com>
2018-05-11 15:12:47 +0200

    triangle

--- srcs/main.c
+++ srcs/main.c
@@ -61,8 +61,64 @@ int			my_keyrelease_funct(int keyrelease, t_env *a)
 	return (0);
 }
 
+void display(void)
+{
+	float points[] = {
+		0.0f,  0.5f,  0.0f,
+		0.5f, -0.5f,  0.0f,
+		-0.5f, -0.5f,  0.0f
+	};
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	const char* vertex_shader =
+	"#version 400\n"
+	"in vec3 vp;"
+	"void main() {"
+	"  gl_Position = vec4(vp, 1.0);"
+	"}";
+
+	const char* fragment_shader =
+	"#version 400\n"
+	"out vec4 frag_colour;"
+	"void main() {"
+	"  frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+	"}";
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	GLuint shader_programme = glCreateProgram();
+	glAttachShader(shader_programme, fs);
+	glAttachShader(shader_programme, vs);
+	glLinkProgram(shader_programme);
+
+	// wipe the drawing surface clear
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(shader_programme);
+	glBindVertexArray(vao);
+	// draw points 0-3 from the currently bound VAO with current in-use shader
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+}
+
 void	ft_start_screen(t_env a)
 {
+
+
 	ft_putstr("lol\n");
 	if (a.x > L || a.x < 0 || a.y > H || a.y < 0 || H < 1 || L < 1)
 		exit(0);
@@ -73,15 +129,14 @@ void	ft_start_screen(t_env a)
 	mlx_opengl_window_set_context(a.win);
 	ft_putendl((char *)glGetString(GL_RENDERER));
 	ft_putendl((char *)glGetString(GL_VERSION));
-	// a.image = mlx_new_image(a.mlx, L, H);
-	// mlx_hook(a.win, 2, (1L << 0), my_key_funct, &a);
-	// mlx_hook(a.win, 3, (1L << 1), my_keyrelease_funct, &a);
-	// // ft_expose(&a);
-	// mlx_loop(a.mlx);
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LESS);
+
+	display();
+
 	mlx_opengl_swap_buffers(a.win);
 	mlx_hook(a.win, 2, (1L << 0), my_key_funct, &a);
+	// mlx_hook(a.win, 3, (1L << 1), my_keyrelease_funct, &a);
 	mlx_loop(a.mlx);
 }
 
